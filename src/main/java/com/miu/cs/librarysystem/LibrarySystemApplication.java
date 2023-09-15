bdb697454505456e92d4dec8ebd5219adba83dca
Khanh Nguyen <pkhanhit@gmail.com>
2023-09-15 13:10:29 -0500

    chore: add inserting test data support

--- src/main/java/com/miu/cs/librarysystem/LibrarySystemApplication.java
+++ src/main/java/com/miu/cs/librarysystem/LibrarySystemApplication.java
@@ -1,8 +1,12 @@
 package com.miu.cs.librarysystem;
 
+import com.miu.cs.librarysystem.dataaccess.DataAccessFacade;
+import com.miu.cs.librarysystem.dataaccess.TestData;
 import com.miu.cs.librarysystem.system.Util;
 import com.miu.cs.librarysystem.ui.window.LoginWindow;
 import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
 
 public class LibrarySystemApplication {
   public static void main(String[] args) {
@@ -10,6 +14,13 @@ public class LibrarySystemApplication {
   }
 
   private static void run() {
+    try {
+      if (Objects.requireNonNull(DataAccessFacade.getDataPath().toFile().list()).length == 0) {
+        TestData.insertTestData();
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
     LoginWindow loginWindow = new LoginWindow();
     Util.centerFrameOnDesktop(loginWindow);
     loginWindow.setVisible(true);
